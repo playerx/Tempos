86677b9ca5150c9e804bdfcf9ee38e83b38419fe
Ezeki Zibzibadze <ezeki@jok.ge>
2012-07-28 23:52:22 +0300

    first

--- MvcApplication2/Global.asax.cs
+++ MvcApplication2/Global.asax.cs
@@ -12,11 +12,15 @@ namespace MvcApplication2
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
+
+
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+
+            int it = 23;
 
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
